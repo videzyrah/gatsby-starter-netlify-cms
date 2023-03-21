b2f15b99a05d0b8c3ce393ba67b8ab2a20b9289d
Greg Stull <videzyrah@gmail.com>
2023-03-21 09:35:11 -0400

    test13

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,6 @@ import { Link } from "gatsby";
 
 import logo from "../img/logo.svg";
 import facebook from "../img/social/facebook.svg";
-import instagram from "../img/social/instagram.svg";
-import twitter from "../img/social/twitter.svg";
-import vimeo from "../img/social/vimeo.svg";
 
 const Footer = () => {
   
@@ -50,32 +47,10 @@ const Footer = () => {
                 </section>
               </div>
               <div className="column is-4 social">
-                <a title="facebook" href="https://facebook.com">
+                <a title="facebook" href="https://www.facebook.com/MealsForMyself/">
                   <img
                     src={facebook}
                     alt="Facebook"
-                    style={{ width: "1em", height: "1em" }}
-                  />
-                </a>
-                <a title="twitter" href="https://twitter.com">
-                  <img
-                    className="fas fa-lg"
-                    src={twitter}
-                    alt="Twitter"
-                    style={{ width: "1em", height: "1em" }}
-                  />
-                </a>
-                <a title="instagram" href="https://instagram.com">
-                  <img
-                    src={instagram}
-                    alt="Instagram"
-                    style={{ width: "1em", height: "1em" }}
-                  />
-                </a>
-                <a title="vimeo" href="https://vimeo.com">
-                  <img
-                    src={vimeo}
-                    alt="Vimeo"
                     style={{ width: "1em", height: "1em" }}
                   />
                 </a>
